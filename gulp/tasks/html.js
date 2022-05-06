e547cf54c2dd62e35eca03b1b740430e552a59ef
g4rv <skydorw@gmail.com>
2022-05-06 20:25:29 +0300

    project base

---
+++ gulp/tasks/html.js
@@ -0,0 +1,43 @@
+import fileInclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
+
+export const html = () => {
+    return app.gulp.src(app.path.src.html)
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "HTML",
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(fileInclude())
+        .pipe(app.plugins.replace(/@img\//g, 'assets/img/'))
+        .pipe(app.plugins.replace(/@js\//g, 'assets/js/'))
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                webpHtmlNosvg()
+            )
+        )
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                versionNumber({
+                    'value': '%DT%',
+                    'append': {
+                        'kay': '_v',
+                        'cover': 0,
+                        'to': [
+                            'css',
+                            'js',
+                        ]
+                    },
+                    'output': {
+                        'file': 'gulp/version.json'
+                    }
+                })
+            )
+        )
+        .pipe(app.gulp.dest(app.path.build.html))
+        .pipe(app.plugins.browserSync.stream());
+}
---
+++ gulp/tasks/scss.js
@@ -0,0 +1,63 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css'; // min css
+import webpcss from 'gulp-webpcss'; // WEBP img
+import autoprefixer from 'gulp-autoprefixer'; // autoprefixer
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // media queries
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+    return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+        .pipe(app.plugins.srcMap.init())
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SCSS",
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                groupCssMediaQueries()
+            )
+        )
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                webpcss({
+                    webpClass: ".webp",
+                    noWebpClass: ".no-webp"
+                })
+            )
+        )
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ["last 3 versions"],
+                    cascade: true
+                })
+            )
+        )
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                cleanCss()
+            )
+        )
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(app.plugins.srcMap.write('./'))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.browserSync.stream());
+}
