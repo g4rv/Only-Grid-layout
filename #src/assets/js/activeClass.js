a34ce8b1e978cdd5e3fa005c11a91fae0525c4aa
g4rv <skydorw@gmail.com>
2022-05-11 12:43:14 +0300

    layout done. But need some fixes)

--- #src/assets/js/activeClass.js
+++ #src/assets/js/activeClass.js
@@ -1,13 +1,32 @@
 const actiiveClass = () => {
     const 
+        //header elements
         header = document.querySelector('.header'),
         burgerBtn = header.querySelector('.burger'),
+        //sub-header nav elements
         headerNav = header.querySelector('.nav'),
-        navLinks = headerNav.querySelectorAll('.nav__link');
+        navLinks = headerNav.querySelectorAll('.nav__link'),
+        //form elements
+        form = document.querySelector('.form'),
+        formSwitches = form.querySelectorAll('.form__switch');
 
     const toggleActiveClass = (obj) => {
         obj.classList.toggle('active');
     }
+
+    const removeActiveClass = (obj) => {
+        obj.classList.remove('active');
+    }
+
+    const addActiveClass = (obj) => {
+        obj.classList.add('active');
+    }
+
+    formSwitches.forEach(formSwitch => {
+        formSwitch.addEventListener('click', () => {
+            toggleActiveClass(formSwitch);
+        })
+    })
 
     navLinks.forEach(navLink => {
         const navLinkHref = navLink.href;
